51ec18659b07a37a4049df83add2881511fdcbbb
Yuchen Xin <e0838400@u.nus.edu>
2022-07-18 22:37:49 +0800

    update the project structure

---
+++ xyc-mall-parent/mall-common/src/test/java/com/xyc/mall/common/MallCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyc.mall.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
